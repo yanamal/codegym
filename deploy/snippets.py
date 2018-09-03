27ebf2c5abef48e34d2aff39e7ff56f78d9d5ae6
Yana <kuziavra@gmail.com>
2018-09-03 15:18:54 -0500

    Write and read snippet data in NDB

--- deploy/snippets.py
+++ deploy/snippets.py
@@ -1,6 +1,9 @@
 
-from flask import Flask,redirect,render_template,request
+from flask import Flask,redirect,render_template,request, Response
 import logging, os
+from google.appengine.ext import ndb
+
+### Post/Get handling
 
 app = Flask(__name__)
 
@@ -13,11 +16,16 @@ if not production_environment:
 
 @app.route('/snippet/new', methods=['POST'])
 def addSnippet():
-    logging.info(request.data)
-    return request.data
+    new_snippet = Snippet(code=request.data)
+    new_snippet_key = new_snippet.put()
+    return str(new_snippet_key.id())
 
-'''
-@app.route('/snippet', methods=['GET']')
-def formresponse():
-  return 'test'
-'''
+@app.route('/snippet/<snipid>', methods=['GET'])
+def formresponse(snipid):
+    snip = Snippet.get_by_id(long(snipid))
+    return Response(snip.code, mimetype='text/coffeescript')
+
+### NDB Storage
+class Snippet(ndb.Model):
+    timestamp = ndb.DateTimeProperty(auto_now_add=True) # timestamp. auto_now_add makes it so that the timestamp is automatically added when an instance is created.
+    code = ndb.StringProperty()
